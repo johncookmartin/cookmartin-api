c9b769ea36685dd1ea3bd19c7fbc6d3135ce68b7
John Cook-Martin <johncookmartin@gmail.com>
2026-03-08 09:48:58 -0400

    Adding unique index do notecards

--- CookMartin.Db/Tables/note.Notecards.sql
+++ CookMartin.Db/Tables/note.Notecards.sql
@@ -2,10 +2,15 @@ CREATE TABLE [note].[Notecards]
 (
     [NotecardId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [CollectionId] INT NOT NULL,
-    [FrontDescription] NVARCHAR(MAX) NOT NULL,
+    [FrontDescription] NVARCHAR(400) NOT NULL,
     [BackDescription] NVARCHAR(MAX) NOT NULL,
     [CreatedDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
     [UpdatedDate] DATETIME2 NULL,
     [IsDeleted] BIT NOT NULL DEFAULT 0,
     CONSTRAINT [FK_Notecards_Collections] FOREIGN KEY ([CollectionId]) REFERENCES [note].[Collections]([CollectionId])
 );
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Notecards_CollectionId_FrontDescription_IsActive] ON [note].[Notecards] ([CollectionId], [FrontDescription]) 
+WHERE [IsDeleted] = 0;
